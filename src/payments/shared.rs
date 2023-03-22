79f41562a89b7186f2e7e5eb63a9b9a1d80d3572
Maxim Gorin <maksim.g@simple.app>
2023-03-22 14:19:39 +0300

    Fix model

--- src/payments/shared.rs
+++ src/payments/shared.rs
@@ -2,12 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct CryptoPaymentAssetNosqlModel {
-    #[serde(rename = "RowKey")]
-    pub row_key: String,
-    #[serde(rename = "PartitionKey")]
-    pub partition_key: String,
-    #[serde(rename = "TimeStamp")]
-    pub timestamp: String,
     #[serde(rename = "AssetSymbol")]
     pub asset_symbol: String,
     #[serde(rename = "BlockchainSymbols")]
