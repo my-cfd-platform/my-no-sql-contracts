ee568f4522d78c0b096c9f1e96885db775bd5ce1
Maxim Gorin <maksim.g@simple.app>
2023-07-25 12:56:28 +0300

    Change payment asset fees

--- src/payments/shared.rs
+++ src/payments/shared.rs
@@ -12,17 +12,8 @@ pub struct CryptoPaymentAssetNosqlModel {
     pub min_amount: Option<f64>,
     #[serde(rename = "MaxAmount")]
     pub max_amount: Option<f64>,
-    #[serde(rename = "FeeAmount")]
-    pub fee_amount: Option<f64>,
-    #[serde(rename = "FeeType")]
-    pub fee_type: i32,
-    #[serde(rename = "MinFeeAmount")]
-    pub min_fee_amount: Option<f64>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub enum FeeTypeNosql {
-    None = 0,
-    Absolute = 1,
-    Percent = 2,
+    #[serde(rename = "FeeFixedAmount")]
+    pub fee_fixed_amount: Option<i32>,
+    #[serde(rename = "FeePercent")]
+    pub fee_percent: Option<f64>,
 }
