4a125ab19ee4464970ce24540c0bfbd8cbfc8246
Maxim Gorin <maksim.g@simple.app>
2023-11-23 16:48:29 +0300

    Fix build

--- src/candles_import.rs
+++ src/candles_import.rs
@@ -1,15 +1,9 @@
 service_sdk::macros::use_my_no_sql_entity!();
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
 #[my_no_sql_entity("candles-import-tasks")]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct CandlesImportTaskNosqlModel {
-    #[serde(rename = "RowKey")]
-    pub row_key: String,
-    #[serde(rename = "PartitionKey")]
-    pub partition_key: String,
-    #[serde(rename = "TimeStamp")]
-    pub timestamp: String,
     #[serde(rename = "Instrument")]
     pub instrument: String,
     #[serde(rename = "CandleType")]
