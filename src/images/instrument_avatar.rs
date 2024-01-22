20328f234d682e10fb13f6a92b1c2252c843c0d6
Maxim Gorin <maksim.g@simple.app>
2024-01-22 16:58:37 +0300

    Fix build for img

--- src/images/instrument_avatar.rs
+++ src/images/instrument_avatar.rs
@@ -1,8 +1,8 @@
 service_sdk::macros::use_my_no_sql_entity!();
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
 #[my_no_sql_entity("instrumentsavatar")]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct InstrumentAvatarNosqlModel {
     #[serde(rename = "TimeStamp")]
     pub timestamp: String,
