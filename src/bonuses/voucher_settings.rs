572756c23168b31bbec0e70a43c6b07e8e96472c
Maxim Gorin <onlinexmax@gmail.com>
2024-07-01 16:56:39 +0300

    Fix naming

--- src/bonuses/voucher_settings.rs
+++ src/bonuses/voucher_settings.rs
@@ -4,21 +4,21 @@ service_sdk::macros::use_my_no_sql_entity!();
 
 #[my_no_sql_entity("bonus-voucher-settings")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct VoucherSettings {
+pub struct VoucherSettingsNosqlModel {
     pub name: String,
     pub asset_amount: f64,
     pub asset_symbol: String,
     pub lifetime_minutes: u64,
-    pub tasks: Vec<VoucherTask>,
+    pub tasks: Vec<VoucherTaskNosqlModel>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct VoucherTask {
+pub struct VoucherTaskNosqlModel {
     pub task_type: i32,
     pub task_amount: f64,
 }
 
-impl VoucherSettings {
+impl VoucherSettingsNosqlModel {
     pub fn generate_pk() -> &'static str {
         "*"
     }
