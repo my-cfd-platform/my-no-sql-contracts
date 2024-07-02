4d407f6b07fcd7e0524c9225d71ee80eca6cc5b0
Maxim Gorin <onlinexmax@gmail.com>
2024-07-02 19:07:03 +0300

    Change voucher settings

--- src/bonuses/voucher_settings.rs
+++ src/bonuses/voucher_settings.rs
@@ -5,17 +5,32 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[my_no_sql_entity("bonus-voucher-settings")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct VoucherSettingsNosqlModel {
+    #[serde(rename = "Name")]
     pub name: String,
-    pub asset_amount: f64,
-    pub asset_symbol: String,
+    #[serde(rename = "Reward")]
+    pub reward: VoucherRewardNosqlModel,
+    #[serde(rename = "LifetimeMinutes")]
     pub lifetime_minutes: u64,
+    #[serde(rename = "Tasks")]
     pub tasks: Vec<VoucherTaskNosqlModel>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct VoucherRewardNosqlModel {
+    #[serde(rename = "Type")]
+    pub reward_type: i32,
+    #[serde(rename = "Amount")]
+    pub amount: f64,
+    #[serde(rename = "Asset")]
+    pub asset: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct VoucherTaskNosqlModel {
+    #[serde(rename = "Type")]
     pub task_type: i32,
-    pub task_amount: f64,
+    #[serde(rename = "Goal")]
+    pub goal: f64,
 }
 
 impl VoucherSettingsNosqlModel {
